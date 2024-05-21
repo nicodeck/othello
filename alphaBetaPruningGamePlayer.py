7d20d1c660e92d0b9bfce8ea5f44f366d041f0c2
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-05-21 17:34:43 +0200

    Add alpha beta pruning game player

---
+++ alphaBetaPruningGamePlayer.py
@@ -0,0 +1,73 @@
+import time
+import random
+from game import Game
+from gamePlayer import GamePlayer
+from randomGamePlayer import RandomGamePlayer
+from naiveGamePlayer import NaiveGamePlayer
+
+class AlphaBetaPruningGamePlayer:
+    def __init__(self):
+        pass
+
+    def playNAlphaBetaPruningGames(self, N, depth):
+        playerOneWins = 0
+        playerTwoWins = 0
+        draws = 0
+        start = time.perf_counter()
+        for i in range(N):
+            print("Game {}/{}".format(i, N))
+            winner = self.playAlphaBetaPruningGame(depth)
+            if winner == 1:
+                playerOneWins += 1
+            elif winner == 2:
+                playerTwoWins += 1
+            elif winner == 0:
+                draws += 1
+            else:
+                raise Exception("Unexpected game output")
+        end = time.perf_counter()
+        print("{} games played: {} wins, {} losses, {} draws, duration: {} seconds per game".format(N, playerOneWins, playerTwoWins, draws, (end - start) / N))
+        
+
+    def playAlphaBetaPruningGame(self, depth, printGrids = False, printValues = False):
+        game = Game()
+        playTheNextTurn = True
+        while playTheNextTurn:
+            if game.getNextPlayer() == 1:
+                playTheNextTurn = AlphaBetaPruningGamePlayer.playAlphaBetaPruningTurn(game, depth, printGrids, printValues)
+            else:
+                playTheNextTurn = RandomGamePlayer.playRandomTurn(game)
+        game.printGrid()
+        return game.getWinner()
+
+    @staticmethod
+    def playAlphaBetaPruningTurn(game: Game, depth, printGrid = False, printTurnValue = False):
+        playableSquares = game.getPlayableSquares()
+        if len(playableSquares) == 0:
+            return False
+        gamePlayer = GamePlayer()
+        node = gamePlayer.buildTreeOfDepthN(game.getGrid(), game.getNextPlayer(), depth)
+        node.recGetAlphaBetaPruningTurn(float("-inf"), float("inf"))
+        children = node.getChildren()
+        bestChildIndexes = [0]
+        bestScore = 0
+        for childIndex in range(len(children)):
+            childScore = children[childIndex].getValue()
+            if childScore > bestScore:
+                bestChildIndexes = [childIndex]
+                bestScore = childScore
+            elif childScore == bestScore:
+                bestChildIndexes.append(childIndex)
+        #print("len of children is " + str(len(children)))
+        bestChild = children[random.choice(bestChildIndexes)]
+        bestSquare = bestChild.getTurn()
+        game.play(bestSquare[0], bestSquare[1])
+        if printGrid:
+            game.printGrid()
+        return True
+
+
+gameplayer = AlphaBetaPruningGamePlayer()
+
+
+gameplayer.playNAlphaBetaPruningGames(100, 3)
